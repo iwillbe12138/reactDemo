2b149987ca12569b0d6867ec20a378c04789c2ae
iwillbe12138 <iwillbe12138@163.com>
2021-03-13 23:31:37 +0800

    2021-03-13

--- src/Xiaojiejie.js
+++ src/Xiaojiejie.js
@@ -2,69 +2,71 @@
  * @Description:demo
  * @Author: iwillbe12138
  * @Date: 2021-03-05 01:02:17
- * @LastEditTime: 2021-03-09 10:01:51
- * @LastEditors:
+ * @LastEditTime: 2021-03-13 23:21:31
+ * @LastEditors: iwillbe12138
  */
 
-import React, { Component, Fragment } from 'react';
-import './style.css';
+import React, { Component, Fragment } from "react";
+import "./style.css";
 //Fragment  碎片  可以最外层不用div
 class Xiaojiejie extends Component {
-    render() {
-        return (
-            //flex
-            <Fragment>
-                <div>
-                    <input
-                        className="input"
-                        value={this.state.inputValue}
-                        onChange={this.inputChange.bind(this)}
-                    ></input>
-                    <button onClick={this.addList.bind(this)}>增加服务</button>
-                </div>
-                <ul>
-                    {this.state.list.map((item, index) => {
-                        return (
-                            <li
-                                onClick={this.deleteItem.bind(this, index)}
-                                key={index + item}
-                                dangerouslySetInnerHTML={{ __html: item }} //html解析
-                            >
-                                {/* {item} */}
-                            </li>
-                        );
-                    })}
-                </ul>
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      //flex
+      <Fragment>
+        <div>
+          <label htmlFor="will">增加服务：</label>
+          <input
+            id="will"
+            className="input"
+            value={this.state.inputValue}
+            onChange={this.inputChange.bind(this)}
+          ></input>
+          <button onClick={this.addList.bind(this)}>增加服务</button>
+        </div>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return (
+              <li
+                onClick={this.deleteItem.bind(this, index)}
+                key={index + item}
+                dangerouslySetInnerHTML={{ __html: item }} //html解析
+              >
+                {/* {item} */}
+              </li>
+            );
+          })}
+        </ul>
+      </Fragment>
+    );
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputValue: '',
-            list: ['基础按摩', '精油推背'],
-        };
-    }
-    inputChange(e) {
-        this.setState({
-            inputValue: e.target.value,
-        });
-        // console.log(e.target.value);
-    }
-    addList() {
-        this.setState({
-            list: [...this.state.list, this.state.inputValue],
-            inputValue: '',
-        });
-    }
-    deleteItem(index) {
-        console.log(index);
-        let list = this.state.list;
-        list.splice(index, 1);
-        this.setState({
-            list: list,
-        });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: ["基础按摩", "精油推背"],
+    };
+  }
+  inputChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+    });
+    // console.log(e.target.value);
+  }
+  addList() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: "",
+    });
+  }
+  deleteItem(index) {
+    console.log(index);
+    let list = this.state.list;
+    list.splice(index, 1);
+    this.setState({
+      list: list,
+    });
+  }
 }
 export default Xiaojiejie;
---
+++ src/text.js
@@ -0,0 +1,21 @@
+/*
+ * @Description: simple react snippets
+ * @Author: iwillbe12138
+ * @Date: 2021-03-13 23:25:03
+ * @LastEditTime: 2021-03-13 23:29:20
+ * @LastEditors: iwillbe12138
+ */
+
+import React, { Component } from 'react';
+
+class text extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default text;
